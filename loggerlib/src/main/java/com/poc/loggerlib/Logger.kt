03abb5b77366c71b5a627d8616a290e007ece477
TUSHAR JAIN <tjain@yamaha-motor-india.com>
2025-05-28 10:19:33 +0530

    Initial loggerlib commit

---
+++ loggerlib/src/main/java/com/poc/loggerlib/Logger.kt
@@ -0,0 +1,26 @@
+package com.example.loggerlib
+
+import android.util.Log
+
+object Logger {
+    var isEnabled = true
+    var globalTag: String = "LoggerLib"
+
+    fun debug(tag: String, message: String) {
+        if (isEnabled) {
+            Log.d(globalTag, "[$tag] $message")
+        }
+    }
+
+    fun info(tag: String, message: String) {
+        if (isEnabled) {
+            Log.i(globalTag, "[$tag] $message")
+        }
+    }
+
+    fun error(tag: String, message: String) {
+        if (isEnabled) {
+            Log.e(globalTag, "[$tag] $message")
+        }
+    }
+}
